710cb6a824b3f7b0c88f36fc979882c51f4e1d0b
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-12 18:16:01 +0600

    add

--- Day_1.sh
+++ Day_1.sh
@@ -28,3 +28,27 @@ do
     fi
 
 done
+
+
+
+
+
+#print the odd number 1 - 10 
+
+
+for (( i=0; i<=10; i++ ))
+do
+        if (( i % 2 != 0 )); then
+          echo $i
+        fi
+done
+
+
+#print the Even number 1 - 10 
+
+for (( i=0; i<=10; i++ ))
+do 
+     if (( i % 2 == 0 )); then 
+      echo $i
+     fi
+done
--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -1,9 +1,6 @@
-for (( i=1; i<=10; i++ ))
+for (( i=0; i<=10; i++ ))
 do 
-    if (( i % 2 == 0 )); then
-        echo "$i the number is odd"
-    else 
-        echo "$i  the number is even "
-    fi
-
+     if (( i % 2 == 0 )); then 
+      echo $i
+     fi
 done
