084388f0bf5a32c13038793c3c5a7c954e2ec885
AyonDevNet <rmcoder7@gmail.com>
2025-04-20 14:39:29 +0600

    add

--- Day_2.sh
+++ Day_2.sh
@@ -6,3 +6,39 @@ do
     sum=$(( sum+num ))
 done
  echo "this is the number" $sum
+
+#Find Maximum in Array
+
+nums=(23 45 12 78 34 99)
+max=${nums[0]}
+
+for num in ${nums[@]};
+do
+   if (( num > max ));
+     then
+      max=$num
+   fi
+done
+echo "Maximum value : " $max
+
+
+#Check if Element Exists
+
+names=("alice" "bob" "charlie")
+read -p "Enter the name : " input
+found=0
+for name in ${name[@]}
+do
+  if [[ $name == $input ]];
+    then
+       found=1
+       break
+   fi
+done
+
+if (( found == 1 ));
+    then
+    echo find the name $input
+  else
+       echo your name is not foud
+fi
